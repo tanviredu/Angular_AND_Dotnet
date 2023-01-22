60a2abe960811bf0bfcab983a6aaf9b9e4adf5de
tanviredu <tanviredu2018@gmail.com>
2023-01-22 23:46:31 +0600

    added

--- 1_ANGULAR_GS/apm-new/src/app/app.component.ts
+++ 1_ANGULAR_GS/apm-new/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'pm-root',
+  template:`<div>
+    <h1>{{pageTitle}}</h1>
+  </div>`
+
 })
-export class AppComponent {
-  title = 'apm-new';
+export class AppComponent{
+  pageTitle:string = "My First Component"
 }
