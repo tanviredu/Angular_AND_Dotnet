cc3dc7cdd326bad7db81e4a5cdd3855bec800999
tanviredu <tanviredu2018@gmail.com>
2023-01-23 15:46:58 +0600

    added

--- 1_ANGULAR_GS/apm-new/src/app/app.component.ts
+++ 1_ANGULAR_GS/apm-new/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
+
 @Component({
   selector:'pm-root',
   template:`<div>
     <h1>{{pageTitle}}</h1>
+    <pm-product-list></pm-product-list>
   </div>`
-
 })
+
 export class AppComponent{
-  pageTitle:string = "My First Component"
+  pageTitle:string = "Acme Product Management"
 }
