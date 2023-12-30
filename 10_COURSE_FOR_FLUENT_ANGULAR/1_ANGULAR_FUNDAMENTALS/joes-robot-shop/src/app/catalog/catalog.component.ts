e227155cce1e6ca0e014d6b05c37a30c1707832f
tanviredu <tanviredu2018@gmail.com>
2023-12-30 10:29:59 +0600

    added

---
+++ 10_COURSE_FOR_FLUENT_ANGULAR/1_ANGULAR_FUNDAMENTALS/joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Iproduct } from './product.model';
+
+@Component({
+  selector: 'app-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+  public product: Iproduct;
+  constructor(){
+    this.product = {
+      id:2,
+      name:'friendly bot',
+      description: "A friendly robot head with two eyes \
+      and smile -- great for domenstic use",
+      imageName:"head-friendly.png",
+      category : "Heads",
+      price: 945.0,
+      discount : 0.2
+    }
+  }
+
+}
